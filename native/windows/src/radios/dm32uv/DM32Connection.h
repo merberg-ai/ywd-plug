3213cc998e4d25f1232282d4d32ea7dce50863cc
merberg <j1mb3rg@gmail.com>
2026-08-29 15:51:27 -0700

    feat(win): add selective read result

--- native/windows/src/radios/dm32uv/DM32Connection.h
+++ native/windows/src/radios/dm32uv/DM32Connection.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "DM32MemoryBlock.h"
+
 #include <QByteArray>
 #include <QString>
+#include <QVector>
 
 #include <functional>
 
@@ -30,12 +33,32 @@ struct DM32BackupResult
     qint64 bytesRead {0};
 };
 
+struct DM32SelectiveReadResult
+{
+    bool ok {false};
+    QString model;
+    QString portName;
+    QString firmware;
+    QString error;
+    quint32 configStart {0};
+    quint32 configEnd {0};
+    int discoveredBlockCount {0};
+    int channelCount {0};
+    int dataBlockCount {0};
+    qint64 bytesTransferred {0};
+    qint64 elapsedMs {0};
+    QVector<DM32MemoryBlock> blocks;
+};
+
 class DM32Connection final
 {
 public:
     using ProgressCallback = std::function<void(int progress, const QString &message)>;
 
     static DM32ProbeResult probe(const QString &portName);
+    static DM32SelectiveReadResult readChannelsSelective(
+        const QString &portName,
+        const ProgressCallback &onProgress = {});
     static DM32BackupResult readRawBackup(
         const QString &portName,
         const QString &outputDirectory,
