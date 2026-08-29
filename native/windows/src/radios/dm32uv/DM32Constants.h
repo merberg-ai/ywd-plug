5da0ecd0de682b71294f8f1363e1e33e79c6f968
merberg <j1mb3rg@gmail.com>
2026-08-29 11:20:42 -0700

    feat(win): add safe DM32 read timing constants

--- native/windows/src/radios/dm32uv/DM32Constants.h
+++ native/windows/src/radios/dm32uv/DM32Constants.h
@@ -8,4 +8,7 @@ inline constexpr int InitDelayMs = 400;
 inline constexpr int ClearBufferDelayMs = 200;
 inline constexpr int PsearchReadDelayMs = 150;
 inline constexpr int RequestTimeoutMs = 5000;
+inline constexpr int ReadMemoryTimeoutMs = 15000;
+inline constexpr int BlockReadDelayMs = 150;
+inline constexpr qsizetype BlockSize = 4096;
 }
