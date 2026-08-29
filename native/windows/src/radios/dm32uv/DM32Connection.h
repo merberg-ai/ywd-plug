2f67f93e23bbd63ccc5928dbd5ac19af3492c9df
merberg <j1mb3rg@gmail.com>
2026-08-29 11:20:24 -0700

    feat(win): add read-only DM32 raw backup API

--- native/windows/src/radios/dm32uv/DM32Connection.h
+++ native/windows/src/radios/dm32uv/DM32Connection.h
@@ -3,6 +3,8 @@
 #include <QByteArray>
 #include <QString>
 
+#include <functional>
+
 struct DM32ProbeResult
 {
     bool ok {false};
@@ -12,8 +14,30 @@ struct DM32ProbeResult
     QByteArray psearchResponse;
 };
 
+struct DM32BackupResult
+{
+    bool ok {false};
+    QString model;
+    QString portName;
+    QString firmware;
+    QString error;
+    QString backupPath;
+    QString manifestPath;
+    QString sha256;
+    quint32 configStart {0};
+    quint32 configEnd {0};
+    int blockCount {0};
+    qint64 bytesRead {0};
+};
+
 class DM32Connection final
 {
 public:
+    using ProgressCallback = std::function<void(int progress, const QString &message)>;
+
     static DM32ProbeResult probe(const QString &portName);
+    static DM32BackupResult readRawBackup(
+        const QString &portName,
+        const QString &outputDirectory,
+        const ProgressCallback &onProgress = {});
 };
