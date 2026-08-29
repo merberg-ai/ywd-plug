e8b635eff823e00ca9a5ac4a310f0d0af7686a9d
merberg <j1mb3rg@gmail.com>
2026-08-29 15:22:22 -0700

    feat(win): add native DM-32UV channel decoder interface

---
+++ native/windows/src/radios/dm32uv/DM32ChannelDecoder.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <QString>
+#include <QVector>
+
+struct DM32ChannelInfo
+{
+    int number {0};
+    QString name;
+    double rxFrequency {0.0};
+    double txFrequency {0.0};
+    bool txDisabled {false};
+    QString mode;
+    QString power;
+    QString bandwidth;
+    bool scanAdd {false};
+    int scanListId {0};
+    int colorCode {-1};
+    int timeSlot {0};
+    int rxGroupListId {-1};
+    int txContactIndex {-1};
+    bool txContactDigital {false};
+};
+
+struct DM32ChannelDecodeResult
+{
+    bool ok {false};
+    QString error;
+    int channelCount {0};
+    QVector<DM32ChannelInfo> channels;
+};
+
+class DM32ChannelDecoder final
+{
+public:
+    static DM32ChannelDecodeResult decodeFile(const QString &path);
+};
